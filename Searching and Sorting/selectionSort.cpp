b67e7e6b9724e8d3bbc26746ccffd042cedcc136
Akash <chakash793@gmail.com>
2025-07-18 08:41:52 +0530

    Prac: Selection Sort

--- Searching and Sorting/selectionSort.cpp
+++ Searching and Sorting/selectionSort.cpp
@@ -2,28 +2,39 @@
 using namespace std;
 
 int selectionSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        int min = i;
-        for (int j = i + 1; j < size; j++) {
-            if (arr[j] < arr[min]) {
-                min = j;
-            }
-        }
-        
-        swap(arr[min], arr[i]);
-    }
-}
+
+
+    for(int i = 0; i < size - 1; i++) {
+        int minIndex=i;
+        for(int j = i+1;j<size;j++) {
+           if(arr[j] < arr[minIndex]){
+            minIndex = j;
+              }
+          }
+          int temp= arr[minIndex];
+          arr[minIndex]=arr[i];
+          arr[i] = temp;
+      }
+
+      cout << "Sorted array: ";
+      for(int i = 0; i < size; i++) {
+          cout << arr[i] << " ";
+      }
+      cout << endl;
+   }
 
 int main() {
-    int arr[6] = {1, 2, 34, 4, 221, 23};
+    int size; 
+    
 
-    selectionSort(arr,6);
+    cout <<"Enter the size of the array: "<<endl;
+    cin >> size;
+    int arr[size];
 
-    cout << "Sorted array: ";
-    for (int i = 0; i < 6; i++) {
-        cout << arr[i] << " ";
+    cout <<"Enter the elements of the array: "<<endl;
+    for(int i=0; i<size; i++) {
+        cin >> arr[i];
     }
-    cout << endl;
-
+    selectionSort(arr, size);
     return 0;
 }
